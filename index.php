264a0a2d310703be7aa26616f21e29f7f40d46db
JauniKappauni <social.jauni@gmail.com>
2025-03-13 14:35:23 +0100

    Added includes

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
+<?php
+    include("includes/header.html");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JauniKappauni.de</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h1>JauniKappauni.de</h1>
-    <a href="register.php">Register</a>
-    <br>
-    <a href="login.php">Login</a>
 </body>
 </html>
+<?php
+    include("includes/footer.html");
+?>
