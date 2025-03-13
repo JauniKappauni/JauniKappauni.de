7aa4d41281801c730d6ff4765dba4683164f6728
JauniKappauni <social.jauni@gmail.com>
2025-03-13 14:37:37 +0100

    Removed some registration parameters Added includes Implemented register functionality/logic

--- register.php
+++ register.php
@@ -1,40 +1,57 @@
+<?php   
+    include("database.php");
+?>
+<?php
+    include("includes/header.html");
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-    <h1>Register</h1>
-    <a href="index.php">Back</a>
-    <form>
-        <label for="firstname">First Name</label>
-        <br>
-        <input type="text" id="firstname" required>
-        <br>
-        <label for="lastname">Lastname</label>
-        <br>
-        <input type="text" id="lastname" required>
-        <br>
-        <label for="birthdate">Birth Date</label>
-        <br>
-        <input type="date" id="birthdate" required>
-        <br>
-        <label for="username">Username</label>
-        <br>
-        <input type="text" id="username" required>
-        <br>
-        <label for="email">Email-Address</label>
-        <br>
-        <input type="text" id="email" required>
-        <br>
-        <label for="password">Password</label>
-        <br>
-        <input type="password" id="password" required>
-        <br>
-        <input type="submit" value="Register">
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <h2>Register</h2>
+        username:<br>
+        <input type="text" name="username"><br>
+        password:<br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="submit" value="register">
     </form>
+    
 </body>
 </html>
+<?php
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if(empty($username)){
+            echo"Please enter a username";
+        }
+        elseif(empty($password)){
+            echo"Please enter a password";
+        }
+        else{
+            $sql = "INSERT INTO users (user, password)
+                    VALUES ('$username', '$password')";
+
+            try{
+                mysqli_query($conn, $sql);
+                echo"You are now registered";
+            }
+            catch(mysqli_sql_exception){
+                echo"That username is taken";
+            }
+            
+        }
+    }
+    mysqli_close($conn);
+?>
+<?php
+    include("includes/footer.html");
+?>
